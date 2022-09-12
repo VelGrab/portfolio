795be4abbde5afc0ca73e1a265888e90a7eac4ef
Velgrab <velgrab@gmail.com>
2022-09-12 19:18:25 -0300

    Update Folder Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import style from './Projects.module.css'
-import FolderProjects from '../../images/folder_desktop.png'
+import React from "react";
+import style from "./Projects.module.css";
+import FolderProjects from "../../images/folder_desktop.png";
 import Home from "../Home/Home";
-import NomadePortfolio from "../../images/Nomade_Portfolio.PNG";
-import VideogamesPortfolio from "../../images/videogamesPortfolio.PNG";
-import ratingPortfolio from "../../images/ratingPortfolio.PNG";
+import NomadePortfolio from "../../images/Nomade_Portfolio2.png";
+import VideogamesPortfolio from "../../images/videogamesPortfolio2.PNG";
+import ratingPortfolio from "../../images/ratingPortfolio2.PNG";
 import { Link } from "react-router-dom";
 
 export default function Projects() {
@@ -13,33 +13,58 @@ export default function Projects() {
       <Home />
       <div className={style.containerFolder}>
         <div className={style.containerLineSup}>
-          <img className={style.folderImgInside} src={FolderProjects} alt="Folder Inside" />
+          <img
+            className={style.folderImgInside}
+            src={FolderProjects}
+            alt="Folder Inside"
+          />
           <p className={style.folderNameInside}>Projects</p>
           <Link className={style.link} to="/home">
             <button className={style.buttonFolder}>X</button>
           </Link>
         </div>
         <div className={style.containerProjects}>
-          <a className={style.targetA} href='https://nomade-khaki.vercel.app/' target='__blank'>
           <div className={style.containerOne}>
-            <img className={style.nomadeImg} src={NomadePortfolio} alt='Nomade Img'></img>
-            <p className={style.projectName}>Nomade</p>
+            <a
+              className={style.targetA}
+              href="https://nomade-khaki.vercel.app/"
+              target="__blank"
+            >
+              <img
+                className={style.nomadeImg}
+                src={NomadePortfolio}
+                alt="Nomade Img"
+              ></img>
+            </a>
           </div>
-          </a>
-          <a className={style.targetA} href='https://videogames-pi-ten.vercel.app/' target='__blank'>
           <div className={style.containerTwo}>
-            <img className={style.nomadeImg} src={VideogamesPortfolio} alt='Videogames Img'></img>
-            <p className={style.projectName}>Games Box</p>
+            <a
+              className={style.targetA}
+              href="https://videogames-pi-ten.vercel.app/"
+              target="__blank"
+            >
+              <img
+                className={style.nomadeImg}
+                src={VideogamesPortfolio}
+                alt="Videogames Img"
+              ></img>
+            </a>
           </div>
-          </a>
-          <a className={style.targetA} href='https://velgrab.github.io/Interactive-rating-component-main/' target='__blank'>
           <div className={style.containerThree}>
-            <img className={style.nomadeImg} src={ratingPortfolio} alt='Rating Img'></img>
-            <p className={style.projectName}>Rating Component</p>
+            <a
+              className={style.targetA}
+              href="https://velgrab.github.io/Interactive-rating-component-main/"
+              target="__blank"
+            >
+              <img
+                className={style.nomadeImg}
+                src={ratingPortfolio}
+                alt="Rating Img"
+              ></img>
+            </a>
           </div>
-          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
